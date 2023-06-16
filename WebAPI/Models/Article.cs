553369fb42d36aab5b3fcc6e94d819c6a1fbf43c
Marcos Leite <m.abner@live.co.uk>
2023-06-16 17:48:26 -0300

    adds active? field and default values for createdat and updated at

--- WebAPI/Models/Article.cs
+++ WebAPI/Models/Article.cs
@@ -10,10 +10,11 @@ namespace WebAPI.Models
         public int Id { get; set; }
         public int CategoryId { get; set; }
         public int AuthorId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
         public string? Title { get; set; }
         public string? Content { get; set; }
         public int views { get; set; }
+        public bool IsActive { get; set; } = true;
     }
 }
