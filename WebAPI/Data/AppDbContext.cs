94a98cc80b495f61ea2bb503326ae294db252c43
Marcos Leite <m.abner@live.co.uk>
2023-06-16 22:18:09 -0300

    app dbContext

---
+++ WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=dotKB.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
